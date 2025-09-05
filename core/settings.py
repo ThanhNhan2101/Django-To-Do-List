d8917e313e79a38f0c1d7b6ee210905c3089be7f
ThanhNhan2101 <nhannguyen210110@gmail.com>
2025-09-05 14:34:30 +0700

    deploy

--- core/settings.py
+++ core/settings.py
@@ -93,16 +93,15 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME', 'TodoProject'),
-        'USER': os.getenv('DB_USER', 'postgres'),
-        'PASSWORD': os.getenv('DB_PASSWORD', '1021012003'),
-        'HOST': os.getenv('DB_HOST', 'db'), 
-        'PORT': os.getenv('DB_PORT', '5432'),
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST", "db"),
+        "PORT": os.getenv("DB_PORT", "5432"),
     }
 }
-
 
 
 
